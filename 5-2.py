d4ee63d642b835946258b50c0b3ae61cb2e13f43
CJA <119636877+anotherarnold@users.noreply.github.com>
2022-12-05 09:01:47 -0500

    Update 5-2.py
    i learned stuff

--- 5-2.py
+++ 5-2.py
@@ -1,20 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Dec  5 00:22:01 2022
-
 @author: CJA
 """
-data = open("input.txt")
-
-#data = open("sample.txt")
-
-file = ""
-
-for line in data.readlines():    
-    file += line
-
-data.close()
-
 def headerget(f):
     header = ""
     for line in f.splitlines():
@@ -29,31 +17,21 @@ def commandgrab(f):
     commands= []
     for line in f.splitlines():
         if len(line) > 0 and line[0] == "m":
-            s = line.replace("move ", "")
-            s = s.replace("from ", "")
-            s = s.replace("to ", "")
-            c = s.split(" ")  
-            d= []
-            for n in c:
-                d.append(int(n))
-            commands.append(d)
+            s = line.replace("move ", "").replace("from ", "").replace("to ", "")
+            c = list(map(lambda x: int(x), s.split(" ")))
+            commands.append(c)
     return commands
 
 def boattracker(h):
-    boats = []
-    i = 0
-    while i < h.index("\n"):
-        if i % 4 == 0:
-            boats.append([])
-        i += 1
+    boats = [[] for x in range(h.index("\n")) if x % 4 == 0]
     for line in h.splitlines():
         i = 0
-        k = 0
+        b = 0
         while i < len(line):
             if i % 4 == 0:
                 if line[i].isalpha():
-                    boats[k].append(line[i])
-                k += 1
+                    boats[b].append(line[i])
+                b += 1
             i += 1
     return boats
 
@@ -63,20 +41,16 @@ def movers(boat, mov):
         while i < move[0]:
             x = boat[move[1]-1].pop(0)
             boat[move[2]-1].insert(i, x)
-            i += 1
-            
+            i += 1          
+
+data = open("input.txt")
+
+file = "".join(line for line in data.readlines())
+
+data.close()
 
 boats = boattracker(headerget(file))
 
-moves = commandgrab(file)
+movers(boats, commandgrab(file))
 
-movers(boats, moves)
-
-answer = ""
-for boat in boats:
-    answer += boat[0]
-
-print(answer)
-
-
-    
+print("".join(boat[0] for boat in boats))
