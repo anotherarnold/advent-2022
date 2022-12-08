0fda9442a5e6c5382fa332221c324c4e7cdfdf6d
CJA <119636877+anotherarnold@users.noreply.github.com>
2022-12-07 23:03:43 -0500

    Add files via upload

---
+++ 7-1.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  7 09:23:55 2022
+
+@author: CJA
+"""
+#I had to get hints for this one
+#I had three different solutions, one brute forced, one recursive, and one using 
+#a tree, that all worked on the sample input but not the full input.
+#so I had to figure out this simpler strategy
+
+#data = open("sampleTwo.txt").read()
+#data = open("sample.txt").read()
+data = open("input.txt").read()
+
+path = []
+#I don't use dicts enough!
+tree = {}
+
+for line in data.splitlines():
+    if line == "$ cd ..":
+        path.pop()
+        #I had forgotten continue was a keyword in python lol
+        continue
+    
+    if line[:4] == "$ cd":
+        path.append(line[5:])
+        
+        if "".join(path) not in tree.keys():
+            tree.update({ "".join(path): 0 })
+        
+        continue
+    
+    if line[0].isnumeric():
+        size = int(line[:line.index(" ")])
+        pwd = []
+        
+        for d in path:
+            pwd.append(d)
+            tree.update({ ''.join(pwd): tree[''.join(pwd)] + size})
+            #print(tree)
+            
+        continue
+
+print(sum([val for val in tree.values() if val < 100000]))
